b377272ce05200aaa34be668b76f19b0fae820b7
Christoph Trautwein <trautw@ubuntu.skweg24.de>
2013-02-11 20:23:44 +0100

    ui added

--- lib/redirect_backoffice.rb
+++ lib/redirect_backoffice.rb
@@ -1,5 +1,11 @@
 require "redirect_backoffice/version"
 
+set :public_folder, File.dirname(__FILE__) + '/../public'
+
+set :bind, "2001:470:1f15:5a8:20c:29ff:fe21:87aa"
+set :port, 4567
+
+
 module RedirectBackoffice
   # Your code goes here...
 end
